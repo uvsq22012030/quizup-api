2a9357852c8d88c419ebbd75dc4e14f6c9e2f725
uvsq22012030 <72565891+uvsq22012030@users.noreply.github.com>
2021-04-30 06:24:20 +0200

    ✨ connect to db

--- server/config.js
+++ server/config.js
@@ -1,27 +1,31 @@
-const joi = require('@hapi/joi')
-const envs = ['dev', 'test', 'prod']
+const joi = require("@hapi/joi");
+const envs = ["dev", "test", "production"];
 
 // Define config schema
 const schema = joi.object().keys({
-  port: joi.number().default(3000),
-  env: joi.string().valid(...envs).default(envs[0])
-})
+	port: joi.number().default(3000),
+	env: joi
+		.string()
+		.valid(...envs)
+		.default(envs[0]),
+});
 
 // Build config
 const config = {
-  port: process.env.PORT,
-  env: process.env.NODE_ENV
-}
+	port: process.env.PORT,
+	env: process.env.NODE_ENV,
+	// mongoUrl: process.env.MONGO_URL
+};
 
 // Validate config
-const { error, value } = schema.validate(config)
+const { error, value } = schema.validate(config);
 
 // Throw if config is invalid
 if (error) {
-  throw new Error(`The server config is invalid. ${error.message}`)
+	throw new Error(`The server config is invalid. ${error.message}`);
 }
 
 // Add some helper props
-value.isDev = value.env === 'dev'
+value.isDev = value.env === "dev";
 
-module.exports = value
+module.exports = value;
